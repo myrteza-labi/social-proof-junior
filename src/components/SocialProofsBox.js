db9883365c0593e130e76619d76b5a032737bf00
Myrteza <myrteza@hotmail.fr>
2021-10-12 18:43:40 +0200

    :style: (mobile) style done

--- src/components/SocialProofsBox.js
+++ src/components/SocialProofsBox.js
@@ -3,6 +3,9 @@ import './SocialProofsBox.css';
 function SocialProofsBox(props){
     return(
         <div className="SocialProofsBox" >
+            <div className="bgImageTop" ></div>
+            <div className="bgImageBottom" ></div>
+
             {props.children}
         </div>
     )
